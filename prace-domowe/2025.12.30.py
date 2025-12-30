daa3f6316cbc729b86eacfd43cdf4922d2cd34ab
Szymon Kaszuba-Gałka <szymonkaszuba3@wp.pl>
2025-12-30 18:51:47 +0100

    update

---
+++ prace-domowe/2025.12.30.py
@@ -0,0 +1,27 @@
+# Napisz program w ktorym obliczysz wartosc 
+# funkcji liniowej dla dwoch wartosci x_1 oraz x_2
+# Wzor funkcji to f(x) = 2*x - 17
+# Badane punkty to:
+# - x_1 = 13
+# - x_2 = 7
+#
+# Nie musisz tworzyc zadnych funkcji dodatkowych, 
+# co chce zebys zrobila to wypisala wzor funkcji 
+# podstawiajac w miejsce x odpowiednia zmienna (x_1 lub x_2)
+# oraz zapisala wartosci funkcji dla danego 
+# punktu x do nowej zmiennej, tzn.
+# - dla x_1 zapisz wynik funkcji do nowej zmiennej o nazwie y_1
+# - dla x_2 zapisz wynik funkcji do nowej zmiennej o nazwie y_2
+# 
+# Wypisz obie wartosci y_1 oraz y_2 oraz wylicz i wypisz ich:
+# - sume
+# - roznice
+# - iloczyn (mnozenie)
+# - iloraz (dzielenie)
+# - iloraz calkowity (dzielenie calkowite)
+# - modulo (reszte z dzielenia)
+#
+# Jezeli zadanie jest zbyt pogmatwane lub czegos nie 
+# rozumiesz, napisz do mnie, sprobuje pomoc 
+# 
+# Powodzenia :)
