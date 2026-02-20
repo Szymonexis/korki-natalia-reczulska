15ebde92f529ddd94fb3a5883cffdee8a051b735
Szymonexis <szymonkaszuba3@wp.pl>
2026-02-20 08:22:33 +0100

    create 2026.02.18 homework

---
+++ prace-domowe/2026.02.18.py
@@ -0,0 +1,67 @@
+"""
+========================================
+ZADANIE 1 - Funkcja print i jej parametry
+========================================
+Za pomoca funkcji print oraz jej parametrow sep i end wypisz nastepujacy tekst
+DOKLADNIE w takiej formie (lacznie z nowa linia na koncu):
+
+poniedziałek -> wtorek -> środa -> czwartek -> piątek!
+
+Uzyj jednego wywolania funkcji print i przekaz nazwy dni tygodnia jako osobne argumenty.
+Twoje rozwiazanie:
+"""
+
+
+"""
+========================================
+ZADANIE 2 - Zmienne i print
+========================================
+Stworz zmienne przechowujace nastepujace informacje o sobie:
+  - imie
+  - nazwisko
+  - ulubiony kolor
+  - ulubiona liczba (jako tekst, np. "7")
+
+Nastepnie uzywajac TYLKO tych zmiennych (bez wpisywania tekstu bezposrednio do printa)
+wypisz zdanie w formacie:
+
+Nazywam sie [imie] [nazwisko], moj ulubiony kolor to [kolor], a ulubiona liczba to [liczba].
+
+Do tego zadania mozesz uzyc dowolnej liczby wywolan funkcji print.
+
+Twoje rozwiazanie:
+"""
+
+
+"""
+========================================
+ZADANIE 3 - Funkcja input i zmienne
+========================================
+Napisz program, ktory:
+1. Pyta uzytkownika o jego imie
+2. Pyta uzytkownika o jego ulubione miasto
+3. Wypisuje wiadomosc w formacie:
+
+Czesc [imie]! Twoje ulubione miasto to [miasto].
+
+Twoje rozwiazanie:
+"""
+
+
+"""
+========================================
+ZADANIE 4 - Tabelka
+========================================
+Za pomoca funkcji print oraz znakow specjalnych \t i \n wypisz nastepujaca tabelke
+DOKLADNIE w takiej formie:
+
+Imie       |   Wiek    |   Miasto
+Anna       |   25      |   Warszawa
+Bartek     |   30      |   Krakow
+Celina     |   22      |   Gdansk
+
+Mozesz uzyc wiecej niz jednego wywolania funkcji print.
+Uzyj parametru sep funkcji print.
+
+Twoje rozwiazanie:
+"""
